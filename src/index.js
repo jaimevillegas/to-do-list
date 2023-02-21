3a01b4bce8632b59bee0b27027967c3ca0da321f
Jaime Villegas <jaimevillegas296@gmail.com>
2023-02-21 14:16:25 -0500

    Work on Dynamic content

--- src/index.js
+++ src/index.js
@@ -1,14 +1,76 @@
-// import _ from 'lodash';
 import './style.css';
 
-function component() {
-  // const element = document.createElement('div');
+const tasksList = document.getElementById('tasks-list');
 
-  // Lodash, now imported by this script
-  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+class Task {
+  constructor(description, completed, id) {
+    this.description = description;
+    this.completed = completed;
+    this.id = id;
+  }
 
-  // return element;
-  return 0;
+  // static tasks = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+  static tasks = [];
+
+  updateList() {
+    tasksList.innerHTML = '';
+    Task.tasks.forEach((task) => {
+      tasksList.insertAdjacentHTML('beforeend', `
+        <div class='task-item'>
+        <p>check</p>
+        <input value='${task.description}'>
+        <p>more-options</p>
+        </div>
+        `);
+    });
+  }
+
+  addTask() {
+    Task.tasks.push(this);
+    // localStorage.setItem('tasks', JSON.stringify(Task.tasks));
+    this.updateList();
+  }
 }
 
-document.body.appendChild(component());
+new Task().updateList();
+
+// FOR TESTING DYNAMIC UPDATE OF THE LIST
+// ADD FIVE TASKS TO THE LIST
+
+window.addEventListener('load', () => {
+  console.log("Hello there!");
+
+  new Task('Task number 1', false, 0).addTask();
+
+  new Task('Task number 2', true, 1).addTask();
+
+  new Task('Task number 3', true, 2).addTask();
+
+  new Task('Task number 4', true, 3).addTask();
+
+  new Task('Task number 5', true, 4).addTask();
+});
+
+// ----- FONTAWESOME -----
+// import '@fortawesome/fontawesome-free/js/fontawesome'
+// import '@fortawesome/fontawesome-free/js/solid'
+// import '@fortawesome/fontawesome-free/js/regular'
+// import '@fortawesome/fontawesome-free/js/brands'
+
+// import "@fortawesome/fontawesome-free/js/all.js";
+// import "@fortawesome/fontawesome-free/css/all.css";
+// import '@fortawesome/fontawesome-free/js/solid'
+
+//
+// require("font-awesome-webpack");
+// import { library, dom } from "@fortawesome/fontawesome-free/js/all.js";
+// import { faCheck } from "@fortawesome/free-solid-svg-icons/faCheck";
+//
+//
+// library.add(faCheck);
+// dom.watch();
+// import './app';
+// import './sass/app.sass';
+// import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
+// import '../node_modules/@fortawesome/fontawesome-free/js/all.js';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
--- webpack.config.js
+++ webpack.config.js
@@ -13,11 +13,30 @@ module.exports = {
     static: './dist',
   },
   module: {
+    loaders: [
+      // the url-loader uses DataUrls.
+      // the file-loader emits files.
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+    ],
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.(svg|eot|woff|woff2|ttf)$/,
+        use: ['file-loader']
+      }
     ],
   },
   plugins: [
