9e12b56e29394883fe1bd6c79ff7640be9a8eace
Jaime Villegas <jaimevillegas296@gmail.com>
2023-02-21 15:53:32 -0500

    Work on Linter errors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import './style.css';
 
-// ----- FONTAWESOME -----
-// import '@fortawesome/fontawesome-free/js/fontawesome'
-// import '@fortawesome/fontawesome-free/js/solid'
-// import '@fortawesome/fontawesome-free/js/regular'
-// import '@fortawesome/fontawesome-free/js/brands'
-
 const tasksList = document.getElementById('tasks-list');
 
 class Task {
